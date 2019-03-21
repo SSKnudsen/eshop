20456782cca90d6b6a381a3bf723dca73d0151c3
Stig Schmidt Knudsen <SSKNotes@hotmail.com>
2019-03-21 12:45:07 +0100

    Test Project start

--- WebstoreConsole/Program.cs
+++ WebstoreConsole/Program.cs
@@ -10,7 +10,7 @@ namespace WebstoreConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!1");
             Console.ReadKey();
         }
     }
